a2ff90bc2314d0fbf2c28c8b224bdcb9d04e0e55
Strker58 <58742932+Strker58@users.noreply.github.com>
2021-10-07 17:55:28 +0530

    changes in enemy script

--- Doofus and Pulpit/Assets/Scripts/Pulpit/makeCubesDisappear.cs
+++ Doofus and Pulpit/Assets/Scripts/Pulpit/makeCubesDisappear.cs
@@ -7,26 +7,27 @@ public class makeCubesDisappear : MonoBehaviour
     public float min_pulpit_destroy_time = 4f;
     public float max_pulpit_destroy_time = 5f;
     public float pulpit_spawn_time = 2.5f;
+    public float[] possiblepossitionsx = {0f, -1.003f, 1.003f};
+    public float[] possiblepossitionsz = { 0f, -1.008f, 1.008f };
     public GameObject reference;
     private int possiblecube = 2;
     private int count=0;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        if (count < 2) StartCoroutine(Spawn());
-        else StartCoroutine(destroyed());
+        StartCoroutine(Spawn());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (count < 2) StartCoroutine(Spawn());
+        else StartCoroutine(destroyed());
     }
     IEnumerator Spawn()
     {
-      
        yield return new WaitForSeconds(pulpit_spawn_time);
-       Instantiate(reference, new Vector2(Random.Range(), transform.position.y), Quaternion.identity);
+        Instantiate(reference, new Vector3(Random.Range(), transform.position.y, transform.position.z), Quaternion.identity);
         count++;
     }
     private void OnTriggerEnter2D(Collider2D target)
